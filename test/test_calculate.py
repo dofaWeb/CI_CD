e0e5ac74ceba9d50351cdbc9071d62e42369094b
dofaWeb <gioakim0432004@gmail.com>
2024-12-26 08:51:57 +0700

    Fixed

--- test/test_calculate.py
+++ test/test_calculate.py
@@ -1,7 +1,6 @@
-import unittest
-from calculate import add
+from .. import calculate 
 def test_add():
     a = 1
     b = 1
-    assert add(a, b) == 2
-    assert add(1, 2) == 3
+    assert calculate.add(a, b) == 2
+    assert calculate.add(1, 2) == 3
